4a8a8067053b615516d087f4284c419026aa5938
sevastyanov1982 <sevastyanov1982@gmail.com>
2021-04-06 09:52:14 +0300

    UpDate Radio.java

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -11,7 +11,6 @@ public class Radio {
     public void setStationNumber(int stationNumber) {
         this.stationNumber = stationNumber;
     }
-
     public int getVolume() {
         return volume;
     }
@@ -23,28 +22,29 @@ public class Radio {
     public void stationUp() {
         if (stationNumber == 9) { ///Проверяет, равны ли значения или нет, если да, тогда условие становится истинным и станция становится 0
             stationNumber = 0;
-        } else
-            stationNumber++; ///увеличивает значение на 1
+        } else {
+            stationNumber++;} ///увеличивает значение на 1
+
     }
     public void stationDown() {
         if (stationNumber == 0) {
             stationNumber = 9;
-        } else
-            stationNumber--;
+        } else {
+            stationNumber--;}
     }
 
     public void volumeUp() {
         if (volume == 10) {
             volume = 10;
-        } else
-            volume++;
+        } else {
+            volume++;}
     }
 
     public void volumeDown() {
         if (volume == 0) {
             volume = 0;
-        } else
-            volume--;
+        } else {
+            volume--;}
     }
 
 
