f57d2ed87c819717d56a885a24f3d494710f4f4c
sevastyanov1982 <sevastyanov1982@gmail.com>
2021-04-07 19:03:18 +0300

    Added Constructor

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -7,63 +7,63 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
-    Radio radio = new Radio();
+
 
     @Test
     void shouldStationNext() {   ///Выбор следующей станции
-        radio.setStationNumber(0);
+        Radio radio = new Radio(0, 10);
         radio.stationUp();
         assertEquals(1, radio.getStationNumber());
     }
 
     @Test
     void shouldStationPrev() {  ///Выбор предыдущей станции
-        radio.setStationNumber(5);
+        Radio radio = new Radio(5, 10);
         radio.stationDown();
         assertEquals(4, radio.getStationNumber());
     }
 
     @Test
     void shouldStationUpWhenStationMax() {   ///Переход станции поле максимальной
-        radio.setStationNumber(9);
+        Radio radio = new Radio(10, 10);
         radio.stationUp();
         assertEquals(0, radio.getStationNumber());
     }
 
     @Test
     void shouldStationDownWhenStationMin() {  /// Переход станции после минимальной
-        radio.setStationNumber(0);
+        Radio radio = new Radio (0,10);
         radio.stationDown();
-        assertEquals(9, radio.getStationNumber());
+        assertEquals(10, radio.getStationNumber());
 
     }
 
     @Test
     void shouldSetStationNumber () {     ///Выбор станции
-        radio.setStationNumber(5);
+        Radio radio = new Radio(5,5);
         assertEquals(5, radio.getStationNumber());
 }
     @Test
     void shouldVolumeUp () { ///Увелечение звука на одну единицу
-        radio.setVolume(2);
+        Radio radio = new Radio(2,50);
         radio.volumeUp();
-        assertEquals(3,radio.getVolume());
+        assertEquals(51,radio.getVolume());
     }
     @Test
     void shouldVolumeDown () { ///Уменьшение звука на одну единицу
-        radio.setVolume(4);
+        Radio radio = new Radio(2,50);
         radio.volumeDown();
-        assertEquals(3,radio.getVolume());
+        assertEquals(49,radio.getVolume());
     }
     @Test
     void shouldVolumeUpWhenVolumeMax() { ///Увелечение звука после максимальной громкости (10)
-        radio.setVolume(10);
+        Radio radio = new Radio(2,100);
         radio.volumeUp();
-        assertEquals(10, radio.getVolume());
+        assertEquals(100, radio.getVolume());
     }
     @Test
     void shouldVolumeUpWhenVolumeMin () { ///Уменьшение звука после минимальной громкости (0)
-        radio.setVolume(0);
+        Radio radio = new Radio(2,0);
         radio.volumeDown();
         assertEquals(0,radio.getVolume());
     }
